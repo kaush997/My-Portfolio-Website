e7d423b4e7c6a28d57ff157a2d4598a480bf004d
Kaushalya Hulathdoowage <skdilukshi97@gmail.com>
2023-10-22 18:35:18 +0530

    commit changes

---
+++ src/components/banner/LeftBanner.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { useTypewriter, Cursor } from "react-simple-typewriter";
+import Media from './Media';
+
+const LeftBanner = () => {
+    const [text] = useTypewriter({
+      words: ["Front-end Developer.", "Full Stack Developer.", "UI/UX Designer."],
+      loop: true,
+      typeSpeed: 20,
+      deleteSpeed: 10,
+      delaySpeed: 2000,
+    });
+  return (
+    <div className="w-full lgl:w-1/2 flex flex-col gap-20">
+      <div className="flex flex-col gap-5">
+        <h4 className=" text-lg font-normal">WELCOME TO MY WORLD</h4>
+        <h1 className="text-6xl font-bold text-white">
+          Hi, I'm <span className="text-designColor capitalize">Kaushalya</span>
+        </h1>
+        <h2 className="text-4xl font-bold text-white">
+          a <span>{text}</span>
+          <Cursor
+            cursorBlinking="false"
+            cursorStyle="|"
+            cursorColor="#ff014f"
+          />
+        </h2>
+        <p className="text-base font-bodyFont leading-6 tracking-wide">
+          I am eager to build a strong foundation in Software Engineering, Machine Learning, and Data Science. My goal is to shape the future of the IT industry through innovation and continuous learning.
+          Join me on this exciting journey, and let's explore the vast possibilities of the digital world together. 
+        </p>
+      </div>
+     {/* Media */}
+     <Media />
+    </div>
+  );
+}
+
+export default LeftBanner
---
+++ src/components/banner/Media.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { FaFacebookF, FaLinkedinIn, FaReact, FaGithub } from "react-icons/fa";
+import { SiTailwindcss, SiFigma, SiPython } from "react-icons/si";
+const Media = () => {
+  return (
+    <div className="flex flex-col xl:flex-row gap-6 lgl:gap-0 justify-between">
+        <div>
+          <h2 className="text-base uppercase font-titleFont mb-4">
+            Find me in
+          </h2>
+          <div className="flex gap-4">
+            <span className="bannerIcon">
+              <FaFacebookF />
+            </span>
+            <span className="bannerIcon">
+              <FaGithub />
+            </span>
+            <span className="bannerIcon">
+              <FaLinkedinIn />
+            </span>
+          </div>
+        </div>
+        <div>
+          <h2 className="text-base uppercase font-titleFont mb-4">
+           SKILL ON
+          </h2>
+          <div className="flex gap-4">
+            <span className="bannerIcon">
+              <FaReact />
+            </span>
+            <span className="bannerIcon">
+              <SiPython />
+            </span>
+            <span className="bannerIcon">
+              <SiTailwindcss />
+            </span>
+            <span className="bannerIcon">
+              <SiFigma />
+            </span>
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default Media
